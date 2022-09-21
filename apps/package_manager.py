539c1fc715cc37942a90eff48eb8f04b3c6b62f5
ica574 <iciliaat@gmail.com>
2022-09-21 22:43:35 +0200

    Added downloader function to retrieve apps from IPFS.

--- apps/package_manager.py
+++ apps/package_manager.py
@@ -3,10 +3,14 @@
 # Author: Isaac Cilia Attard
 # Description: Manages packages and creates objects to be used by the main program from other scripts with standard calls.
 
+import urllib.request
 import importlib
 import json
 
+urllib.request.urlretrieve("https://bafybeiaojcquohxn5abpsczpbllcbfyrl2s3st3lyodhrpwi6jhqj4dbne.ipfs.dweb.link/repository.json", "marketplace/download/repository.json") # Downloads module repository to machine
+
 app_dict = json.load(open("./apps/apps.json", "r"))
+global_dict = json.load(open("./apps/repository.json"))
 
 def install(module_name, class_name, app_cues): # Installs a module into the application dictionary
     app_dict[module_name] = {}
@@ -35,3 +39,10 @@ def factory(module_name, class_name): # Instantiates objects based on the keys c
     app_object = getattr(app_module, class_name)
     app_instance = app_object()
     return app_instance
+
+def download(module): # Downloads a module from IPFS/Filecoin infrastructure
+    for module_name in global_dict.keys():
+        if module_name == module:
+            urllib.request.urlretrieve("https://{identifier}.ipfs.dweb.link/{app}.py".format(identifier=global_dict[module_name]["cid"], app=module_name), "marketplace/upload/{app}.py".format(app=module_name)) # Downloads module to machine
+        else:
+            pass
